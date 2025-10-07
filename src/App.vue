c26027b4cf20a7e5222ad126d72e95aa2bd2c59a
Lucas Barbosa <29305833+lucasbarbosaz@users.noreply.github.com>
2025-10-07 00:05:27 -0300

    diretiva v-for

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,45 @@
 <template>
-  <TheHeader v-if="showHeader" />
+  <div>
 
-  <div v-show="showName">
-    Nome: {{ fistName }} <br>
-    Sobrenome: {{  lastName }}
+
+
+    <div v-for="(obj, index) in array" v-bind:key="obj.id" class="item">
+      {{ index }}. {{ obj.name }}
+    </div>
+
+
+    
   </div>
 
-  <div v-if="accessLevel === 'admin'">Usuário Admin</div>
-  <div v-else-if="accessLevel === 'marketing'">Usuário Marketing</div>
-  <div v-else>Usuário Comum</div>
-
-  <img 
-  alt="Vue logo" 
-  src="./assets/logo.png"
-  >
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import TheHeader from './components/TheHeader.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-    TheHeader
-  },
   data: () => {
     return {
-      showHeader: false,
-      fistName: 'Flavio',
-      lastName: 'Barbosa',
-      showName: true,
-      accessLevel: 'admin' // admin, marketing, user
+      array: [
+        { id: 1, name: 'Item 1' },
+        { id: 2, name: 'Item 2' },
+        { id: 3, name: 'Item 3' },
+        { id: 4, name: 'Item 4' },
+        { id: 5, name: 'Item 5' }
+      ]
     }
   }
 }
 </script>
 
 <style>
+.item {
+  margin: 10px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #f9f9f9;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
