aa213449cdcdef4ec9f2f51db2e634549556cd8e
Lucas Barbosa <29305833+lucasbarbosaz@users.noreply.github.com>
2025-10-06 23:22:01 -0300

    Componentes de arquivo unico SFC

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <header class="header">
+        <h1>My Vue.js App</h1>
+    </header>
+</template>
+
+<script>
+
+export default {
+    name: 'TheHeader'
+}
+</script>
+
+<style scoped>
+.header {
+    background-color: #42b983;
+    padding: 20px;
+    color: white;
+}
+</style>
