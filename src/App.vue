945f584d272486d3c9144f19d778f9b9ce5e592b
Lucas Barbosa <29305833+lucasbarbosaz@users.noreply.github.com>
2025-10-10 20:15:10 -0300

    Atributos Class e Style dinamicos

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
 
+    <h1 :class="{ 'title': true, 'text-home': isHome }">Vue.js 3</h1>
 
+    <p :class="pClass">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, corrupti cumque minus laborum commodi possimus ipsum provident ipsa sint modi, libero est? Consequatur ipsam non voluptates et sint rerum reprehenderit.
+    </p>
+
+    <p :style="styleClass">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quidem iste quam aperiam illum tempora perspiciatis soluta mollitia ipsam quo voluptatem, accusantium magni ratione, vel non quae nisi fugiat aliquam.
+    </p>
 
     <div v-for="(obj, index) in array" v-bind:key="obj.id" class="item">
       {{ index }}. {{ obj.name }}
@@ -20,6 +28,11 @@ export default {
   name: 'App',
   data: () => {
     return {
+      isHome: true,
+      classVar: 'title',
+      pClass: ['text', 'text-home'],
+      title: 'Aprendizagem Vue.js',
+      styleClass: { 'color': 'aqua', backgroundColor: 'black' },
       array: [
         { id: 1, name: 'Item 1' },
         { id: 2, name: 'Item 2' },
@@ -33,6 +46,20 @@ export default {
 </script>
 
 <style>
+.text-home {
+  font-size: 48px;
+  color: #35495e;
+}
+
+.title {
+  color: #42b983;
+  font-size: 32px;
+}
+
+.text {
+  color: gray;
+}
+
 .item {
   margin: 10px;
   padding: 10px;
