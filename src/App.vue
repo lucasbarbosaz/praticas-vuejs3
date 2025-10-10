becb45a493059b684b8090bb780d1b234009596d
Lucas Barbosa <29305833+lucasbarbosaz@users.noreply.github.com>
2025-10-10 20:31:17 -0300

    Ligacao de dados bidirecional (diretiva v model)

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,57 @@
 <template>
   <div>
+    <div>
+      Two way data binding
+      v-model -> formularios
+    </div>
 
-    <h1 :class="{ 'title': true, 'text-home': isHome }">Vue.js 3</h1>
+    <br><br>
 
-    <p :class="pClass">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, corrupti cumque minus laborum commodi possimus ipsum provident ipsa sint modi, libero est? Consequatur ipsam non voluptates et sint rerum reprehenderit.
-    </p>
+    <div>
+      <label for="name">Name: </label>
+      <input v-model="name" type="text" placeholder="Enter your name" /> <br><br>
+      {{ name }}
+    </div>
 
-    <p :style="styleClass">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quidem iste quam aperiam illum tempora perspiciatis soluta mollitia ipsam quo voluptatem, accusantium magni ratione, vel non quae nisi fugiat aliquam.
-    </p>
+    <br /><br />
 
-    <div v-for="(obj, index) in array" v-bind:key="obj.id" class="item">
-      {{ index }}. {{ obj.name }}
+    <div>
+      <label for="sports">Choose a sport: </label>
+      <select v-model="sports">
+        <option disabled value="">Please select one</option>
+        <option value="football">Football</option>
+        <option value="basketball">Basketball</option>
+        <option value="tennis">Tennis</option>
+      </select> <br><br>
+      {{ sports }}
+    </div>
+    <br /><br />
+    <div>
+      <label>Newsletter Subscription: </label>
+      <input v-model="newsletter" type="radio" value="Sim" /> Sim
+      <input v-model="newsletter" type="radio" value="Não" /> Não
+      <br><br>
+      {{ newsletter }}
+    </div>
+
+    <br /><br />
+    <div>
+      <label>Contrato</label>
+      <input v-model="contract" type="checkbox" /> Aceita nossos termos
+      <br><br>
+      {{ contract }}
     </div>
 
 
-    
+    <br /><br />
+    <div>
+      <label>Cores que você mais gosta </label>
+      <input v-model="colors" type="checkbox" value="Vermelho" /> Vermelho
+      <input v-model="colors" type="checkbox" value="Verde" /> Verde
+      <input v-model="colors" type="checkbox" value="Azul" /> Azul
+      <br><br>
+      {{ colors }}
+    </div>
   </div>
 
 </template>
@@ -28,45 +63,17 @@ export default {
   name: 'App',
   data: () => {
     return {
-      isHome: true,
-      classVar: 'title',
-      pClass: ['text', 'text-home'],
-      title: 'Aprendizagem Vue.js',
-      styleClass: { 'color': 'aqua', backgroundColor: 'black' },
-      array: [
-        { id: 1, name: 'Item 1' },
-        { id: 2, name: 'Item 2' },
-        { id: 3, name: 'Item 3' },
-        { id: 4, name: 'Item 4' },
-        { id: 5, name: 'Item 5' }
-      ]
+      name: '',
+      sports: '',
+      newsletter: '',
+      contract: false,
+      colors: []
     }
   }
 }
 </script>
 
 <style>
-.text-home {
-  font-size: 48px;
-  color: #35495e;
-}
-
-.title {
-  color: #42b983;
-  font-size: 32px;
-}
-
-.text {
-  color: gray;
-}
-
-.item {
-  margin: 10px;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  background-color: #f9f9f9;
-}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
