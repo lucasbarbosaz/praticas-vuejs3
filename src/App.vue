a7ea1707546a3a1c289e7be85b8a4455fff38b21
Lucas Barbosa <29305833+lucasbarbosaz@users.noreply.github.com>
2025-10-11 13:12:28 -0300

    Eventos

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,19 @@
 <template>
   <div>
-    <div>
-      Two way data binding
-      v-model -> formularios
+    <button @click.once="onClick">
+      Enviar
+    </button>
+    <br><br>
+
+    <div @mouseover.once="onMouseOver" @mouseout.once="onMouseOut">
+      Passe o mouse aqui
     </div>
 
     <br><br>
-
-    <div>
-      <label for="name">Name: </label>
-      <input v-model="name" type="text" placeholder="Enter your name" /> <br><br>
-      {{ name }}
-    </div>
-
-    <br /><br />
-
-    <div>
-      <label for="sports">Choose a sport: </label>
-      <select v-model="sports">
-        <option disabled value="">Please select one</option>
-        <option value="football">Football</option>
-        <option value="basketball">Basketball</option>
-        <option value="tennis">Tennis</option>
-      </select> <br><br>
-      {{ sports }}
-    </div>
-    <br /><br />
-    <div>
-      <label>Newsletter Subscription: </label>
-      <input v-model="newsletter" type="radio" value="Sim" /> Sim
-      <input v-model="newsletter" type="radio" value="Não" /> Não
-      <br><br>
-      {{ newsletter }}
-    </div>
-
-    <br /><br />
-    <div>
-      <label>Contrato</label>
-      <input v-model="contract" type="checkbox" /> Aceita nossos termos
-      <br><br>
-      {{ contract }}
-    </div>
-
-
-    <br /><br />
-    <div>
-      <label>Cores que você mais gosta </label>
-      <input v-model="colors" type="checkbox" value="Vermelho" /> Vermelho
-      <input v-model="colors" type="checkbox" value="Verde" /> Verde
-      <input v-model="colors" type="checkbox" value="Azul" /> Azul
-      <br><br>
-      {{ colors }}
-    </div>
+    <form action="https://google.com" @submit.prevent="onSubmit">
+      <input type="text" @keyup.enter="onKeyUp" />
+    </form>
   </div>
-
 </template>
 
 <script>
@@ -63,11 +23,28 @@ export default {
   name: 'App',
   data: () => {
     return {
-      name: '',
-      sports: '',
-      newsletter: '',
-      contract: false,
-      colors: []
+      var: 'teste'
+    }
+  },
+  methods: {
+    onClick($evt) {
+      console.log('Clicou', $evt)
+    },
+
+    onMouseOver($evt) {
+      console.log('Passou o mouse', $evt)
+    },
+
+    onMouseOut($evt) {
+      console.log('Tirou o mouse', $evt)
+    },
+
+    onSubmit($evt) {
+      console.log('Enviou o form', $evt)
+    },
+
+    onKeyUp($evt) {
+      console.log('Soltou a tecla', $evt)
     }
   }
 }
